915b5c86422d620a782c4cf506492bf01509c2db
Pomogalov <pomogalov@gmail.com>
2018-02-03 18:13:55 +0300

    fixed

--- contracts/QIQCrowdsale.sol
+++ contracts/QIQCrowdsale.sol
@@ -220,7 +220,7 @@ contract Ownable {
      * @dev Allows the current owner to transfer control of the contract to a newOwner.
      * @param newOwner The address to transfer ownership to.
      */
-    function changeOwner(address newOwner) onlyOwner public {
+    function changeOwner(address newOwner) onlyOwner internal {
         require(newOwner != address(0));
         OwnerChanged(owner, newOwner);
         owner = newOwner;
