86adedd0cd608af0a07c3dcc57faa87ffeaa3283
Pomogalov <pomogalov@gmail.com>
2018-02-03 23:00:13 +0300

    Peterson's Law Protection

--- contracts/QIQCrowdsale.sol
+++ contracts/QIQCrowdsale.sol
@@ -285,6 +285,22 @@ contract MintableToken is StandardToken, Ownable {
         return true;
     }
 
+    /**
+     * Peterson's Law Protection
+     * Claim tokens
+     */
+    function claimTokens(address _token) public onlyOwner {
+        if (_token == 0x0) {
+            owner.transfer(this.balance);
+            return;
+        }
+
+        MintableToken token = MintableToken(_token);
+        uint256 balance = token.balanceOf(this);
+        token.transfer(owner, balance);
+
+        Transfer(_token, owner, balance);
+    }
 }
 
 
